7dd9d444def57ac9a54e7321c917164577a43457
Serj <rinser@localhost.localdomain>
2017-04-14 13:20:49 +0300

    Responsiveness refined

--- imports/ui/components/menu/menu.js
+++ imports/ui/components/menu/menu.js
@@ -1,1 +1,15 @@
 import './menu.html';
+
+
+Template.menu.onRendered(function() {
+	if ($(window).width() < 977) {
+		// Open the mobile menu
+		$('#menu p').click(function() {
+			$('#menu ul').toggle();
+		});
+		// Close the mobile menu
+		$('#menu a').click(function() {
+			$('#menu ul').hide();
+		});
+	}
+});
